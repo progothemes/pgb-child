3492af82b69912c8bcd63cbe93b43ae512eefa55
Tim Spinks <tim@ninthlink.com>
2015-09-22 11:09:09 -0700

    remove_filter( 'the_content', 'wpautop' );

--- functions.php
+++ functions.php
@@ -8,3 +8,5 @@ add_action( 'wp_enqueue_scripts', 'pgb_child_enqueue_styles' );
 function pgb_child_enqueue_styles() {
     wp_enqueue_style( 'pgb', get_template_directory_uri() . '/style.css' );
 }
+
+remove_filter( 'the_content', 'wpautop' );
