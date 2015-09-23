05d86565d5fac3eec34fbfef537bd208fd0ca3f2
Tim Spinks <tim@ninthlink.com>
2015-09-23 09:31:13 -0700

    new custom script

--- functions.php
+++ functions.php
@@ -9,4 +9,10 @@ function pgb_child_enqueue_styles() {
     wp_enqueue_style( 'pgb', get_template_directory_uri() . '/style.css' );
 }
 
+add_action( 'wp_enqueue_scripts', 'pgb_child_enqueue_scripts' );
+function pgb_child_enqueue_styles() {
+    wp_enqueue_script( 'nectar7-js', get_stylesheet_directory_uri() . 'includes/js/nectar7.js', array('jquery') );
+}
+
+
 remove_filter( 'the_content', 'wpautop' );
