2ebc3e433c4c202bad4e00b127899d80a7d81138
AmitDeo <magpie.arise@gmail.com>
2015-10-06 15:43:24 +0545

    Homepage Initial commit

---
+++ block-footercopyright.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Footer Copyright Area
+ *
+ */
+
+$show_footer_copyright = pgb_get_option( 'footer_show_copyright', false );
+$default_copyright = 'Copyright &copy; ' . date( 'Y' ) . ' - ' . get_bloginfo( 'name' );
+
+if( !empty( $show_footer_copyright ) && '1' === $show_footer_copyright ) { ?>
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<?php tha_footer_top(); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-8 col-md-8 pull-right text-right">
+				<div class="socialicons">
+					<ul>
+						<li class="twitter"><a href="#"></a></li>
+						<li class="facebook"><a href="#"></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-4 col-md-4 pull-left">
+				<div class="site-copyright">
+					<?php echo pgb_get_option( 'footer_copyright_text', $default_copyright ); ?>
+				</div><!-- close .site-info -->
+			</div>
+		</div>
+	</div><!-- close .container -->
+	<?php tha_footer_bottom(); ?>
+</footer><!-- close #colophon -->
+<?php } ?>
--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@
 add_action( 'wp_enqueue_scripts', 'pgb_child_enqueue_styles' );
 function pgb_child_enqueue_styles() {
     wp_enqueue_style( 'pgb', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'pgb-fonts', 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,300,300italic' );
 }
 
 add_action( 'wp_enqueue_scripts', 'pgb_child_enqueue_scripts' );
@@ -18,9 +19,9 @@ function pgb_child_enqueue_scripts() {
 function get_woo_cart_menu() {
 	global $woocommerce;
 	if ( sizeof( $woocommerce->cart->cart_contents) > 0 ) :
-		$item = sprintf( '<ul class="nav navbar-nav navbar-right"><li><a href="%s">(%s) items <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a></li></ul>', $woocommerce->cart->get_checkout_url(), $woocommerce->cart->get_cart_contents_count() );
+		$item = sprintf( '<ul class="nav navbar-nav navbar-right"><li class="cartlink"><a href="%s">(%s) items <span class="shopping-cart-icon"></span></a></li></ul>', $woocommerce->cart->get_checkout_url(), $woocommerce->cart->get_cart_contents_count() );
 	else :
-		$item = sprintf( '<ul class="nav navbar-nav navbar-right"><li><a href="%s">Cart empty <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a></li></ul>', $woocommerce->cart->get_checkout_url() );
+		$item = sprintf( '<ul class="nav navbar-nav navbar-right"><li class="cartlink"><a href="%s">(0) Items <span class="shopping-cart-icon"></span></a></li></ul>', $woocommerce->cart->get_checkout_url() );
 	endif;
 	return $item;
 }
