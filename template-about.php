627f1a41328f28838deb02d1ac953ed192e390ec
AmitDeo <magpie.arise@gmail.com>
2015-10-26 07:18:30 +0545

    About Us page

---
+++ template-about.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * Template Name: AboutUs Template
+ *
+ * @package pgb
+ */
+
+get_header(); ?>
+
+	<div id="content" class="main-content-inner col-sm-12 col-md-12 col-lg-12">
+		
+		<?php tha_content_top(); ?>
+
+		<?php // <!--The Loop ?>
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php tha_entry_before(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'row' ); ?>>
+
+					<?php tha_entry_top(); ?>
+
+					<div class="col-md-12 main-content-inner2 aboutuspage">
+
+						<div class="row">
+
+							<?php //get_template_part( 'content', get_post_format() ); ?>
+							<div class="entry-content main-entry-content col-md-12">
+								<div class="row aboutushero about1">
+									<div class="col-xs-12 col-sm-12 col-md-12 text-center">
+										<div class="herocaption">
+											<h1>About</h1>
+											<p>NECTAR7 is a performance-based lifestyle company. We focus on bringing innovative performance based nutritional science and lifestyle supplement products direct to consumers. NECTAR7 uncovers, licenses, formulates and contracts measurable life enhancing ingredients to enhance human performance & longevity.</p>
+										</div>
+									</div>
+								</div>
+								<div class="row aboutushero about2">
+									<div class="col-xs-12 col-sm-12 col-md-12 text-center">
+										<div class="herocaption">
+											<p>Our founders are a select team of business, finance, technology, science, global distribution, and branding. Our team is committed to bringing to market breakthrough products through exclusive relationships with leading innovators.</p>
+										</div>
+									</div>
+								</div>
+								<div class="alternatecols">
+									<div class="row">
+										<div class="col-xs-12 col-sm-4 col-md-4 eachCol">
+											<div class="colcont">
+												<h3>NECTAR7 Global</h3>
+												<p>Global distribution for NECTAR7 products is handled by our team of leaders with over 35 years in retail experience.</p>
+											</div>	
+										</div>
+										<div class="col-xs-12 col-sm-4 col-md-4 eachCol altCol">
+											<div class="colcont">
+												<h3>NECTAR7 Media</h3>
+												<p>Advertising and online media services are delivered from our industry leading partners and alliances in all things digital.</p>
+											</div>	
+										</div>
+										<div class="col-xs-12 col-sm-4 col-md-4 eachCol">
+											<div class="colcont">
+												<h3>NECTAR7 Beverage</h3>
+												<p>Product development for growing trends in consumer beverage are delivered through our beverage division.</p>
+											</div>		
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-xs-12 col-sm-4 col-md-4 eachCol altCol">
+											<div class="colcont">
+												<h3>NECTAR7 Wound & Healing</h3>
+												<p>Distribution and product relationships for latest innovations and technology are handled by our wound healing division.</p>
+											</div>		
+										</div>
+										<div class="col-xs-12 col-sm-4 col-md-4 eachCol">
+											<div class="colcont">
+												<h3>NECTAR7 Research</h3>
+												<p>Breakthroughs in formulation of life changing offerings are directed by our research teams located throughout the world.</p>
+											</div>		
+										</div>
+										<div class="col-xs-12 col-sm-4 col-md-4 eachCol altCol">
+											<div class="colcont">
+												<h3>NECTAR7 Licensing Division</h3>
+												<p>If you are interested in taking to market various products with NECTAR7, our license division is the team.</p>
+											</div>		
+										</div>
+									</div>	
+								</div>
+								<div class="row aboutmission">
+									<div class="col-xs-12 col-sm-7 col-md-7">
+										<p><strong>Our mission</strong> is to help people just like you perform the closet to your peak mental and physical capabilities, that you may have never imagined possible. Our products help customers transition to a performance-based lifestyle with a combination of science-based solutions through dietary lifestyle-supplements backed with extensive scientific research and intellectual property.</p>
+									</div>
+								</div>
+								<div class="row alttwocols">
+									<div class="col-xs-12 col-sm-6 col-md-6 blueeight">
+										<div class="textblock">
+											<h4>Why NECTAR7?</h4>
+											<p>In our lifetime we may never encounter a breakthrough that can enhance our daily performance and mental well being in under 8 hours. The most potent, life enhancing discovery of our lifetime . In 8 hours this process begins.</p>
+										</div>
+									</div>
+									<div class="col-xs-12 col-sm-6 col-md-6 whitecol">
+										<div class="textblock">
+											<h4>Changing Lives</h4>
+											<p>If you love someone… We all know people that are on diabetes, cancer and if we could add one more day of opportunity to there life experiences… Most of use are healthy but are we getting our maximum value. If you want to make a difference in live. Quality of life. I don’t think there is anything worse than feeling capable of something and you are doing the lower end. Even the people that are bumping along and they are holy moly. We want to connect people with a reason, we have a life enhancing ingredient which in itself drives personal performance both mentally & physically.</p>
+										</div>	
+									</div>
+								</div>
+								<div class="row alttwocols">
+									<div class="col-xs-12 col-sm-6 col-md-6 walkblock">
+										
+									</div>
+									<div class="col-xs-12 col-sm-6 col-md-6 colorCol">
+										<div class="textblock">
+											<h4>Nectar 7 (N7) Delivers Seven Vital</h4> 
+											<h4>Mitochondrial Functions</h4>
+											<ul>
+											<li>+ Anti-Aging</li>
+											<li>+ Enhanced Energy Levels</li>
+											<li>+ Endurance</li>
+											<li>+ Weight Management</li>
+											<li>+ Cardiovascular Health</li>
+											<li>+ Cognitive Health</li>
+											<li>+ Protection vs Neurological Disease</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="row aboutproduct">
+									<div class="col-xs-12 col-sm-6 col-md-7 textcol">
+										<p>Nectar Seven is a lifestyle marketing company specializing in performance-based-nutrition for healthy lifestyles. Our signature product, Nectar7 (N7), is an anti-aging tablet featuring Niagen (NAD+) and Collagen. Scientifically formulated to enhance cell activity—rejuvenation, recovery, repair and response while delivering a unrivaled metabolic boost.</p>
+									</div>
+									<div class="col-xs-12 col-sm-6 col-md-5 thumbcol">
+										<img src="<?php bloginfo('stylesheet_directory'); ?>/images/nectar_and_niagen.jpg" class="img-responsove"/>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-xs-12 aboutushero about3">
+										<p>For strategic partnerships and relationships with NECTAR7, both<br/> 
+											domestic and global, please contact us by calling 800-xxx-xxxx.<br/>
+											Thank you for choosing NECTAR7, LLC.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</article><!-- #post-## -->
+
+			<?php endwhile; // end of the loop. ?>
+
+		<?php endif; ?>
+
+		<?php // The Loop--> ?>
+
+		<?php// tha_content_bottom(); ?>
+	
+	</div>
+
+<?php get_footer(); ?>
