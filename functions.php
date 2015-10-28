b599b6afc98266f5b1c2a7b148a98a3f4c4857a7
Tim Spinks <tim@ninthlink.com>
2015-10-28 14:09:10 -0700

    js update

--- functions.php
+++ functions.php
@@ -46,7 +46,7 @@ function nectar7_chat_js() {
 	window.$zopim||(function(d,s){var z=$zopim=function(c)
 	{z._.push(c)}
 	,$=z.s=
-	d.createElement(s),e=d.getElementsByTagName(s)0;z.set=function(o)
+	d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o)
 	{z.set. _.push(o)}
 	;z.=[];z.set.=[];$.async=!0;$.setAttribute("charset","utf-8");
 	$.src="//v2.zopim.com/?3QTM9x4eKtsWWMq3RyePaAEVThayWclE";z.t=+new Date;$.
