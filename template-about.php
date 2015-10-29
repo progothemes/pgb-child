f6af951df93961ad56c8a2d2239bcc67a2b8d857
Tim Spinks <tim@ninthlink.com>
2015-10-29 15:07:11 -0700

    blocks and about copy updates

--- template-about.php
+++ template-about.php
@@ -64,8 +64,6 @@ get_header(); ?>
 												<p>Product development for growing trends in consumer beverage are delivered through our beverage division.</p>
 											</div>		
 										</div>
-									</div>
-									<div class="row">
 										<div class="col-xs-12 col-sm-4 col-md-4 eachCol altCol">
 											<div class="colcont">
 												<h3>NECTAR7 Wound &amp; Healing</h3>
@@ -98,18 +96,18 @@ get_header(); ?>
 											<p>In our lifetime we may never encounter a breakthrough that can enhance our daily performance and mental well being in under 8 hours. The most potent, life enhancing discovery of our lifetime . In 8 hours this process begins.</p>
 										</div>
 									</div>
-									<div class="col-xs-12 col-sm-6 col-md-6 whitecol">
+									<!--div class="col-xs-12 col-sm-6 col-md-6 whitecol">
 										<div class="textblock">
 											<h4>Changing Lives</h4>
 											<p>If you love someone… We all know people that are on diabetes, cancer and if we could add one more day of opportunity to there life experiences… Most of use are healthy but are we getting our maximum value. If you want to make a difference in live. Quality of life. I don’t think there is anything worse than feeling capable of something and you are doing the lower end. Even the people that are bumping along and they are holy moly. We want to connect people with a reason, we have a life enhancing ingredient which in itself drives personal performance both mentally & physically.</p>
 										</div>	
-									</div>
-								</div>
-								<div class="row alttwocols">
+									</div-->>
+								<!--/div>
+								<div class="row alttwocols"-->
 									<div class="col-xs-12 col-sm-6 col-md-6 walkblock">
 										
 									</div>
-									<div class="col-xs-12 col-sm-6 col-md-6 colorCol">
+									<!--div class="col-xs-12 col-sm-6 col-md-6 colorCol">
 										<div class="textblock">
 											<h4>NECTAR7 (N7) Delivers Seven Vital</h4> 
 											<h4>Mitochondrial Functions</h4>
@@ -123,7 +121,7 @@ get_header(); ?>
 											<li>+ Protection vs Neurological Disease</li>
 											</ul>
 										</div>
-									</div>
+									</div-->
 								</div>
 								<div class="row aboutproduct">
 									<div class="col-xs-12 col-sm-6 col-md-7 textcol">
