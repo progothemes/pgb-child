0d540af1370010de98f17dc35fc677cb5256aeb2
Tim Spinks <tim@ninthlink.com>
2015-10-29 15:30:13 -0700

    footer social fix

--- block-footercopyright.php
+++ block-footercopyright.php
@@ -12,16 +12,16 @@ if( !empty( $show_footer_copyright ) && '1' === $show_footer_copyright ) { ?>
 	<?php tha_footer_top(); ?>
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 col-sm-8 col-md-8 pull-right text-right">
+			<div class="col-xs-6 col-sm-6 col-md-6 pull-right text-right">
 				<div class="socialicons">
-					<ul>
+					<ul class="list-inline">
 						<li class="twitter"><a href="http://www.twitter.com/nectarseven"></a></li>
 						<li class="instagram"><a href="https://instagram.com/nectarseven"></a></li>
 						<li class="facebook"><a href="http://www.facebook.com/nectarseven"></a></li>
 					</ul>
 				</div>
 			</div>
-			<div class="col-xs-12 col-sm-4 col-md-4 pull-left">
+			<div class="col-xs-6 col-sm-6 col-md-6 pull-left">
 				<div class="site-copyright">
 					<?php echo pgb_get_option( 'footer_copyright_text', $default_copyright ); ?>
 				</div><!-- close .site-info -->
