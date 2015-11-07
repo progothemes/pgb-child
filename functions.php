0815c76efaf583b545b3d2e4fc2146b36beec791
Tim Spinks <tim@ninthlink.com>
2015-11-06 16:47:13 -0800

    top row container fix

--- functions.php
+++ functions.php
@@ -74,9 +74,11 @@ function get_woo_cart_menu() {
  */
 add_action( 'tha_header_before', 'n7_top_widget_bar' );
 function n7_top_widget_bar() { ?>
-	<div id="top-widget-bar" class="row hidden-xs hidden-sm">
-		<div class="col-md-12 text-center">
-			<?php echo do_shortcode( '[gravityform id="3" title="false" description="true" ajax="true"]' ); ?>
+	<div id="top-widget-bar" class="container-fluid hidden-xs hidden-sm">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<?php echo do_shortcode( '[gravityform id="3" title="false" description="true" ajax="true"]' ); ?>
+			</div>
 		</div>
 	</div>
 <?php }
