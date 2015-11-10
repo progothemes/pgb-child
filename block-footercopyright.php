859a641c2d613db1c3274c6d6a55565a8e0382e3
AmitDeo <magpie.arise@gmail.com>
2015-11-10 16:13:23 +0545

    Copy update

--- block-footercopyright.php
+++ block-footercopyright.php
@@ -29,9 +29,11 @@ if( !empty( $show_footer_copyright ) && '1' === $show_footer_copyright ) { ?>
 		</div>
 		<div class="row">
 			<div class="col-xs-12 disclaimertext">
-				<p>The statements regarding these products have not been evaluated by the Food and Drug Administration.<br/>
-					 These products are not intended to diagnose, treat, cure or prevent any disease. The information on this Web site or in emails is designed for educational purposes only.<br/>  
-					 It is not intended to be a substitute for informed medical advice or care. You should not use this information to diagnose or treat any health problems or illnesses without consulting your doctor.</p>
+				<p><strong>Statements have not been evaluated by the Food and Drug Administration.  These products are not intended to diagnose, treat, cure or prevent any disease.</strong></p>
+				<p>The information provided on this site is for informational purposes only and is not intended as a substitute for advice from your physician or other health care professional or any information contained on or in any product label or packaging.
+				You should not use the information on this or the product described for diagnosis or treatment of any health problem or as a substitute for any prescription of any medication or other treatment.  
+				You should consult with a health care professional before starting any supplementation program, before taking any medication, or if you have or suspect you might have a health problem.  You should not stop taking any medication without first consulting your physician.  
+				<br/>This information and product is not recommended for women who are pregnant or children.   </p>
 			</div>
 		</div>
 	</div><!-- close .container -->
