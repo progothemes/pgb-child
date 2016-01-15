d8b431690ee538ff25e73550ec390eccfbf03445
alex chousmith <alex@ninthlink.com>
2016-01-14 22:48:30 -0800

    that didnt seem to work

--- functions.php
+++ functions.php
@@ -6,10 +6,12 @@
 
 add_action( 'wp_enqueue_scripts', 'pgb_child_enqueue_styles', 11 );
 function pgb_child_enqueue_styles() {
+  /*
   wp_dequeue_style('pgb-bootstrap');
   wp_enqueue_style( 'pgb-bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css' );
   wp_dequeue_style( 'pgb-style' );
   wp_enqueue_style( 'pgb-style', get_stylesheet_directory_uri() . '/css/pgb-style-1.1.1.min.css' );
+  */
   wp_enqueue_style( 'pgb', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'pgb-fonts', 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,300,300italic' );
   wp_enqueue_style( 'nectar7', get_stylesheet_directory_uri() . '/style.min.css' );
