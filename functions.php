39c684c604b5b0acc59f50287d1115559f2f5007
alex chousmith <alex@ninthlink.com>
2016-01-14 21:39:18 -0800

    remove script & stylesheet versions & some other

--- functions.php
+++ functions.php
@@ -15,6 +15,41 @@ function pgb_child_enqueue_scripts() {
     wp_enqueue_script( 'nectar7-js', get_stylesheet_directory_uri() . '/includes/js/nectar7.js', array('jquery') );
 }
 
+// remove wp version param from any enqueued scripts
+function nectar7_remove_wp_ver_css_js( $src ) {
+    if ( strpos( $src, 'ver=' . get_bloginfo( 'version' ) ) )
+        $src = remove_query_arg( 'ver', $src );
+    return $src;
+}
+add_filter( 'style_loader_src', 'nectar7_remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'nectar7_remove_wp_ver_css_js', 9999 );
+
+function nectar7_cleaner_wp_header_please() {
+	// Display the links to the extra feeds such as category feeds
+	remove_action( 'wp_head', 'feed_links_extra', 3 );
+	// Display the links to the general feeds: Post and Comment Feed
+	remove_action( 'wp_head', 'feed_links', 2 );
+	// Display the link to the Really Simple Discovery service endpoint, EditURI link
+	remove_action( 'wp_head', 'rsd_link' );
+	// Display the link to the Windows Live Writer manifest file.
+	remove_action( 'wp_head', 'wlwmanifest_link' );
+	// index link
+	remove_action( 'wp_head', 'index_rel_link' );
+	// prev link
+	remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+	// start link
+	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+	// Display relational links for the posts adjacent to the current post.
+	remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
+	// Display the XHTML generator that is generated on the wp_head hook, WP version
+	remove_action( 'wp_head', 'wp_generator' );
+	// rel-canonical tag
+	remove_action( 'wp_head', 'rel_canonical' );
+	// default wp shortlink meta tag
+	remove_action( 'wp_head', 'wp_shortlink_wp_head' );
+}
+add_action( 'wp_head', 'nectar7_cleaner_wp_header_please', 0 );
+
 /**
  * Google Tag Manager
  *
