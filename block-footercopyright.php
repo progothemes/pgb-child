0adaccfb4d505af3961cf01fb92d0e03cf96e64c
Mitchell Austin <mitchell@ninthlink.com>
2016-01-25 13:17:00 -0800

    revised footer copy with explicit disclaimer added

--- block-footercopyright.php
+++ block-footercopyright.php
@@ -29,11 +29,10 @@ if( !empty( $show_footer_copyright ) && '1' === $show_footer_copyright ) { ?>
 		</div>
 		<div class="row">
 			<div class="col-xs-12 disclaimertext">
-				<p>*Statements have not been evaluated by the Food and Drug Administration.  These products are not intended to diagnose, treat, cure or prevent any disease.</p>
-				<p>The information provided on this site is for informational purposes only and is not intended as a substitute for advice from your physician or other health care professional or any information contained on or in any product label or packaging.
-				You should not use the information on this or the product described for diagnosis or treatment of any health problem or as a substitute for any prescription of any medication or other treatment.  
-				You should consult with a health care professional before starting any supplementation program, before taking any medication, or if you have or suspect you might have a health problem.  You should not stop taking any medication without first consulting your physician.  
-				<br/>This information and product is not recommended for women who are pregnant or children.   </p>
+				<p>*These statements have not been evaluated by the Food and Drug Administration. These products are not intended to diagnose, treat, cure or prevent any disease.</p>
+				<p>DISCLAIMER: Results vary per individual, depending on the context of their circumstance and lifestyle. Testimonials on this site came from real customers who were not paid for their statements. However, each customer experience is unique and you may have an entirely dissimilar experience using the product.</p>
+				<p>The information provided on this site is for informational purposes only and is not medical advice. You should not use the information on this site for diagnosis or treatment of any health problem nor as a substitute for any prescription medication or other treatment. You should consult with a healthcare professional before starting any supplementation program, before taking any medication, or if you have or suspect you might have a health problem. You should not stop taking any medication without first consulting your physician.  
+				<br/>This information and product is not recommended for children or women who are pregnant.</p>
 			</div>
 		</div>
 	</div><!-- close .container -->
