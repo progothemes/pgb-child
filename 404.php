f00b64fe51fc185226a2032eb74e9cf93ed75d09
Mitchell Austin <mitchell@ninthlink.com>
2016-03-23 16:13:41 -0700

    adds 404 page/template

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+	<div id="content" class="main-content-inner col-sm-12 col-md-12 col-lg-12">
+
+		<?php tha_content_top(); ?>
+
+		<h1>Error 404</h1>
+		<p>The requested page/resource is unable to be found. Visit our <a href="/">homepage</a>, or search our site:</p>
+		<?php get_search_form(); ?>
+
+		<?php tha_content_bottom(); ?>
+	</div>
+<?php get_footer(); ?>
