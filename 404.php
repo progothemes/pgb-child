6bbc6b710d3af5659fb427cab99fbdc9e4bfb5cb
Mitchell Austin <mitchell@ninthlink.com>
2016-03-24 19:37:06 -0700

    changes 404 page

--- 404.php
+++ 404.php
@@ -4,38 +4,45 @@
  *
  * @package pgb
  */
+?>
+<style type="text/css">
+	body {
+	    margin: 0;
+	    font-family: "Myriad Pro", Myriad, Frutiger, sans-serif;
+	    background:#e1e1e1;
+	}
+	#contain{
+	    max-width:1240px;
+	    margin:0 auto;
+	    background:#fff;
+	}
+	#logo{
+	    margin:28px 45px;
+	}
+	header{ padding:21px 45px; background: #004B7F linear-gradient(0deg, #6ECDF3,#004B7F) no-repeat center; }
+	header:after{
+		content:'¯\\_(ツ)_/¯';
+		display:block;
+		font-size:4em;
+		font-size:6vw;
+		font-weight: 300;
+		color:#FFF;
+	}
+	h1 small{ font-weight:normal; font-size:.618em; }
+	#message{
+		background-color: #FFF;
+		padding: 21px 45px;
+	}
+</style>
+<div id="contain">
 
-//get_header(); ?>
-	<style type="text/css">
-		.page-heading{ padding:21px 34px; background: #004B7F linear-gradient(0deg, #6ECDF3,#004B7F) no-repeat center; }
-		.page-heading:after{
-			content:'¯\_(ツ)_/¯';
-			display:block;
-			font-size:5em;
-			font-weight: 300;
-			color:#FFF;
-		}
-		h1{ color: #FFF; }
-		.entry-content{
-			background-color: #FFF;
-			padding: 21px 34px;
-		}
-	</style>
-	<div id="content" class="main-content-inner col-sm-12 col-md-12 col-lg-12">
-
-		<?php //tha_content_top(); ?>
-
-		<div class="row">
-			<div class="col-xs-12 page-heading">
-				<h1 class="page-title">Page Not Found: <small>Error 404</small></h1>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-12 entry-content">
-				<p>Nothing can be found at this location. We hope you'll visit our <a href="/">homepage</a> or <a href="/contact-us/">contact us</a>.</p>
-			</div>
-		</div>
-
-		<?php //tha_content_bottom(); ?>
+	<a href="/"><img id="logo" src="/wp-content/uploads/2015/09/logo-Nectar-7.png" alt="Nectar 7" width="226" height="30" /></a>
+	<header>
+		<h1>Not Found <small>Error 404</small></h1>
+	</header>
+	<div id="message">
+		<p>Sorry! Nothing can be found at this location. Perhaps you'll find what you are looking for by <a href="/">visiting our homepage</a> or <a href="/contact-us/">contacting us</a>.</p>
 	</div>
-<?php //get_footer(); ?>
+
+</div>
+
